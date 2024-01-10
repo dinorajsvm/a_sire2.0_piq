d16fe52e4b4e038ebd82a4243a01c11cf2ea9f52
manikandan.sekar <manikandan.sekar@solverminds.sg>
2024-01-10 11:51:42 +0530

    Ui fixes

--- src/app/modules/dashboard/pages/directives/numberSpec.directives.ts
+++ src/app/modules/dashboard/pages/directives/numberSpec.directives.ts
@@ -6,8 +6,8 @@ export class NumberSpecDirective {
   constructor(private _el: ElementRef) {}
   @HostListener('input', ['$event']) onInputChange(event: any) {
     const initalValue = this._el.nativeElement.value;
-    const sanitizedValue = initalValue.replace(/[^0-9.]+/g, '');
-    const numericValue = parseInt(sanitizedValue, 10);
+    const sanitizedValue = initalValue.replace(/[^0-9]/g, '').replace(/^0+/, '');
+    const numericValue = sanitizedValue === '' ? 0 : parseInt(sanitizedValue, 10);
     if (isNaN(numericValue) || numericValue < 0 || numericValue > 48) {
       // If the value is not in the desired range, set it to 0 or 48 accordingly
       this._el.nativeElement.value = Math.min(48, Math.max(0, numericValue));
